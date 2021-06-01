d8670327ee168a3cb5939e1e2332fc9b38e338db
PTAdrian1997 <poptifreaadrian@gmail.com>
2021-06-01 11:32:24 +0300

    Complete the test case for ChessboardStateValidator.nogoodsAreIncompatible

--- src/test/scala/agents/logic/ChessboardStateValidatorTest.scala
+++ src/test/scala/agents/logic/ChessboardStateValidatorTest.scala
@@ -44,10 +44,36 @@ class ChessboardStateValidatorTest extends AnyFeatureSpec with GivenWhenThen wit
       thrown must have message "requirement failed: The augmented agent view must contain the caller agent's id"
     }
     Scenario("The list of nogoods is empty and the augmented view contains the callerId"){
-
+      Given("An empty list of nogoods")
+      val nogoods: Nogoods = EmptyNogoods
+      And("An agent view that contains the caller id")
+      val agentView: LocalView = Map(0 -> 0, 1 -> 3, 2 -> 1, 3 -> 3)
+      val callerId: Int = 0
+      And(s"$callerId as the callerId")
+      When("The nogoodsAreIncompatible method is called")
+      val nogoodsAreIncompatible = PrivateMethod[AcceptanceResponseT](Symbol("nogoodsAreIncompatible"))
+      val actualResult: AcceptanceResponseT = chessboardStateValidator
+        .invokePrivate(nogoodsAreIncompatible(nogoods, agentView, callerId))
+      Then("The result should be the StateValidResponse object")
+      actualResult mustBe StateValidResponse
     }
     Scenario("There's only one nogood that is compatible with the agent view"){
-
+      Given("An augmented agent view")
+      val agentView: LocalView = Map(0 -> 0, 1 -> 3, 2 -> 1, 3 -> 3)
+      val callerId: Int = 0
+      And(s"$callerId as callerId")
+      And("A list of nogoods that only contains one compatible nogood")
+      val nogoods: Nogoods = Array(
+        Nogood(Map(0 -> 0, 3 -> 0)),
+        Nogood(Map(1 -> 3, 3 -> 3)),
+        Nogood(Map(0 -> 0, 1 -> 3, 2 -> 1))
+      )
+      When("The nogoodsAreIncompatible method is called")
+      val nogoodsAreIncompatible = PrivateMethod[AcceptanceResponseT](Symbol("nogoodsAreIncompatible"))
+      val actualResult: AcceptanceResponseT = chessboardStateValidator
+        .invokePrivate(nogoodsAreIncompatible(nogoods, agentView, callerId))
+      Then("The result must be a StateInvalidResponse containing the compatible nogood")
+      actualResult mustBe StateInvalidResponse(Nogood(Map(0 -> 0, 1 -> 3, 2 -> 1)))
     }
     Scenario("There are multiple compatible nogoods of different sizes in the list"){
       Given("An augmented agent view")
